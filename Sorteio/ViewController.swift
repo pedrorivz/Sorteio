84ec9367462391ee375b7cba01c975d4aba57f9e
phsantosit <phsantosit@gmail.com>
2022-02-04 02:55:15 -0300

    Interface and outlet/action on components

--- Sorteio/ViewController.swift
+++ Sorteio/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var resultText: UITextView!
+    
+    @IBAction func GerarNumero(_ sender: Any) {
+        var numero = arc4random_uniform(100)
+        resultText.text = String(numero)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
